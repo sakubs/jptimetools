80b34be319f4fe798c195a69ad51ae0c14a88bac
Brian Sakurada <sakuradabs@gmail.com>
2020-01-20 15:05:16 +0900

    Added some dataclasses for Taisho through Reiwa.

---
+++ jptimetools/era.py
@@ -0,0 +1,49 @@
+"""
+Information relating to Japanese eras.
+"""
+from dataclasses import dataclass
+from datetime import date
+
+CURRENT_YEAR = str(date.today().year)
+
+@dataclass
+class Era:
+    '''
+    This class contains information about Japanese eras
+    '''
+    startyear: str
+    endyear: str
+    kanji: str
+    romaji: str
+
+
+@dataclass
+class Reiwa(Era):
+    startyear: str = '2019'
+    endyear: str = CURRENT_YEAR
+    kanji: str = '令和'
+    romaji: str = 'reiwa'
+
+
+@dataclass
+class Heisei(Era):
+    startyear: str = '1989'
+    endyear: str = '2019'
+    kanji: str = '平成'
+    romaji: str = 'heisei'
+
+
+@dataclass
+class Showa(Era):
+    startyear: str = '1926'
+    endyear: str = '1989'
+    kanji: str = '昭和'
+    romaji: str = 'shouwa'
+
+
+@dataclass
+class Taisho(Era):
+    startyear: str = '1912'
+    endyear: str = '1926'
+    kanji: str = '大正'
+    romaji: str = 'taishou'
---
+++ jptimetools/tests/test_era.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+import sys
+sys.path.append('..')
+
+from era import Reiwa
+
+
+if __name__ == "__main__":
+    pass
